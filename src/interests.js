b33193a1e16b257fe319af98d248434674cfa137
BradHillier <bradhillier@stumail.viu.ca>
2024-03-12 16:11:01 -0700

    Change how animation that activate on scroll work
    - add package framer-motion
    - remove scrollRef
    - remove hidden, visible, and slideIn keyframes
    - wrap interests in motion.div to animate when in view

--- src/interests.js
+++ src/interests.js
@@ -1,4 +1,3 @@
-import { useContext, useRef, useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -7,57 +6,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faNetworkWired } from '@fortawesome/free-solid-svg-icons'
 import { faServer } from '@fortawesome/free-solid-svg-icons'
 import { faMicrochip } from '@fortawesome/free-solid-svg-icons'
+import { motion } from 'framer-motion'
 
-import { ScrollContext } from './App.js';
 
 
 
 const Interest = ({ icon, title, body, index }) => {
 
-    const eleRef = useRef(null);
-    const scrollRef = useRef(0);
-
-    const [isInView, setIsInView] = useState(false);
-
-    useEffect(() => {
-
-      const handleScroll = () => {
-        scrollRef.current = window.pageYOffset;
-        const rect = eleRef.current.getBoundingClientRect();
-        setIsInView(
-            scrollRef.current + window.innerHeight > rect.top + rect.height / 2
-        );
-      };
-
-      if (!isInView) {
-        window.addEventListener('scroll', handleScroll);
-
-        // call handleScroll on page load. If interests are 
-        // on screen upon page render, this triggers their animation
-        if (scrollRef.current == 0) {
-            handleScroll()
-        }
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-      }
-    }, [scrollRef]); // Only re-attach scroll listener when scrollPosition changes
-
-    useEffect(() => {
-        if (isInView) {
-            eleRef.current.style.animation = `appear-below ${0.5 + (index + 1) * 0.15}s ease-in-out`
-            eleRef.current.classList.remove("hidden")
-            eleRef.current.classList.add("visible")
-        }
-    }, [isInView])
-
-
-
     return (
-        <Card ref={eleRef} className="interest hidden border-0 bg-light"> 
+        <Card className="border-0 bg-light">
             <Card.Header className="bg-light border-bottom-0 mt-2">
-                <FontAwesomeIcon 
+                <FontAwesomeIcon
                     className="fa-xl fa-fw text-primary bg-white p-2 rounded shadow-sm"
                     icon={icon} />
             </Card.Header>
@@ -93,19 +52,36 @@ const Interests = () => {
             body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         }
     ]
+    const container = {
+        show: {
+            transition: {
+                delay: 0.5,
+                ease: "easeInOut",
+                delayChildren: 0.1,
+                staggerChildren: 0.1
+            }
+        }
+    }
+
+    const item = {
+        hidden: { opacity: 0, transform: "translateY(40%)" },
+        show: { opacity: 1, transform: "translateY(0)" }
+    }
 
     return (
-        <Row>
+        <motion.div className="row"
+            variants={container}
+            initial="hidden"
+            whileInView="show"
+        >
             {interests.map((area, index) => (
-                <Col 
-                    id={`interest-${index}`} 
-                    md={4} 
-                    key={`interest-${index}`} 
+                <motion.div className="col-md-4"
+                    variants={item}
                 >
                     <Interest index={index} icon={area.icon} title={area.title} body={area.body} />
-                </Col>
+                </motion.div>
             ))}
-        </Row>
+        </motion.div>
     );
 }
 
