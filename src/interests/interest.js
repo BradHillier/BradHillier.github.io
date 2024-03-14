35ee4c5e67ff63b0848f65f2a78578c479219dec
BradHillier <bradhillier@stumail.viu.ca>
2024-03-13 19:54:31 -0700

    Change background colour of interests

--- src/interests/interest.js
+++ src/interests/interest.js
@@ -7,7 +7,7 @@ const Interest = ({ icon, title, body }) => {
         <Card className = "border-0 bg-light" >
             <Card.Header className="bg-light border-bottom-0 mt-2">
                 <FontAwesomeIcon
-                    className="fa-xl fa-fw text-primary bg-white p-2 rounded shadow-sm"
+                    className="fa-xl fa-fw text-info bg-white p-2 rounded shadow-sm"
                     icon={icon} />
             </Card.Header>
             <Card.Body className="pt-0">
