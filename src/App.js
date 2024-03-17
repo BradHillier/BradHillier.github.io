dc5908f488b9fb7ccc16df7e844012804cc229aa
BradHillier <bradhillier@stumail.viu.ca>
2024-03-17 14:29:33 -0700

    Break up header components and add animation
    - Break header into the following components
            - Hook
            - CallToAction
            - CallToActionButton
            - HeroImage
    - Add animation to Hero text components
    - Add image of myself with circular underlay to HeroImage
    - Make navbar translucent

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import Footer from './footer.js';
 import './App.scss';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faNetworkWired, faServer, faMicrochip } from '@fortawesome/free-solid-svg-icons';
+import { faNetworkWired, faServer, faMicrochip, faPlay, faCode, faArrowRight} from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
-library.add(faNetworkWired, faServer, faMicrochip, faGithub);
+library.add(faNetworkWired, faServer, faMicrochip, faPlay, faCode, faGithub, faArrowRight);
 
 
 const App = () => {
--- src/header.js
+++ src/header.js
@@ -1,39 +1,135 @@
 import Container from 'react-bootstrap/Container';
+import Stack from 'react-bootstrap/Stack';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { motion } from 'framer-motion';
 
 
 const Header = () => {
 
-    const adjustScroll = event => {
-        const link_target = document.querySelector('#contact');
+    const container = {
+        show: {
+            transition: { delayChildren: 0.1, staggerChildren: 0.3 }
+        }
+    }
 
-        // The height of the navbar
-        link_target.style.scrollMarginTop = '50px';
+    const fadeIn = {
+        hidden: {
+            opacity: 0
+        },
+        show: {
+            opacity: 1,
+            transition: { ease: "easeInOut", duration: 0.75 }
+        }
+    }
+
+    const fadeUp = {
+        hidden: {
+            opacity: 0, 
+            transform: "translateY(50%)",
+        },
+        show: {
+            opacity: 1,
+            transform: "translateY(0)",
+            transition: { ease: "easeInOut", duration: 1 }
+        }
     };
+
+    const HeroImage = () => {
+
+        const imageStyle = { 
+            borderRadius: "40px", 
+            zIndex: 1,
+        };
+        const underlayCircle = {
+            position: "absolute",
+            left: "50%",
+            top: "50%",
+            transform: "translate(-50%, -50%)",
+            width: "80%",
+            aspectRatio: "1",
+            borderRadius: "50%",
+            backgroundColor: "rgb(230, 40, 114)",
+            zIndex: 0
+        }
+
+        return (
+            <motion.div className="position-relative text-center"> 
+                <Image className="position-relative" src="me.png" fluid style={imageStyle} />
+                <div style={underlayCircle} />
+            </motion.div>
+        );
+    };
+
+
+    const Hook = () => (
+        <motion.h3 variants={fadeIn} className="d-line-block">
+            <span>
+                Hey, I'm Brad, a&nbsp;
+            </span>
+            <span className="d-inline-block">
+                computer science student&nbsp;
+            </span>
+            <span>
+                and aspiring&nbsp;
+            </span>
+            <span className="text-primary d-inline-block">
+                Software Engineer
+            </span>
+        </motion.h3>
+    );
+
+    const CallToAction = () => (
+        <motion.h1 variants={fadeIn}>
+            <div> 
+                Let's build something together!
+            </div>
+        </motion.h1>
+    );
+
+    const HeroSubtext = () => (
+        <motion.h6 variants={fadeUp}>
+            Whether you need a skilled developer or a collaborative team player, I'm eager to discuss how I can be a valuable asset to your project.
+        </motion.h6>
+    );
+
+    const CallToActionButton = () => (
+        <motion.div variants={fadeIn}>
+            <Button href="#contact" variation="primary" size="lg">
+                Get in Touch &nbsp;
+                <FontAwesomeIcon icon={"arrow-right"} />
+            </Button>
+        </motion.div>
+    );
 
     return (
         <header id="home" className="bg-light">
-            <Container className="py-5">
-                <Row className="align-items-center">
-                    <Col>
-                        {/* Hero Text*/} 
-                        <h5 className="text-black">Hey, I'm Brad</h5>
-                        <h1>
-                            <div className="my-3">Aspiring <span className="text-primary">Software Engineer</span></div>
-                            <div className="my-3">Let's build something together!</div>
-                        </h1>
-                        <p className="mb-5 w-75">
-                            Whether you need a skilled developer or a collaborative team player, I'm eager to discuss how I can be a valuable asset to your project. 
-                        </p>
-                        <Button onClick={adjustScroll} href="#contact" variation="primary">Get in Touch</Button>
-                    </Col>
-                    <Col>
-                        <Image src="https://cdn.pixabay.com/photo/2017/02/23/13/05/avatar-2092113_1280.png" fluid />
-                    </Col>
-                </Row>
+            <Container className="py-5 col-xxl-10">
+                <motion.div
+                    className="row align-items-center flex-sm-row-reverse"
+                    variants={container}
+                    initial="hidden"
+                    whileInView="show"
+                >
+                    <Row className="align-items-center flex-lg-row-reverse">
+                        <Col sm={12} md={6} lg={6} className="mb-5">
+                            <HeroImage />
+                        </Col>
+                        <Col sm={12} md={6} lg={6} id="hero-text">
+                            <Stack gap={2}>
+                                <Hook />
+                                <CallToAction />
+                                <Stack gap={5}>
+                                    <HeroSubtext />
+                                    <CallToActionButton />
+                                </Stack>
+                            </Stack>
+                        </Col>
+                    </Row>
+                </motion.div>
             </Container>
         </header>
     );
