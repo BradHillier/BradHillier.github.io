eb7cdd981ee064beb3e0c5354be0ce299af034e6
BradHillier <bradhillier@stumail.viu.ca>
2024-03-17 18:12:28 -0700

    Update title and icon
    - add terminal-icon.svg and terminal-icon.ico files
    - update index.html and manifest.json to use the newly added icons
    - add terminal-icon.svg to navbar

--- src/nav.js
+++ src/nav.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
+import { Image } from 'react-bootstrap';
 
 
 const NavigationBar = () => {
@@ -10,9 +11,9 @@ const NavigationBar = () => {
   const [isNavExpanded, setIsNavExpanded] = useState(false);
 
   const links = {
-    'Home': '#home', 
-    'Interests': '#interests', 
-    'About': '#about', 
+    'Home': '#home',
+    'Interests': '#interests',
+    'About': '#about',
     'Projects': '#projects'
   }
 
@@ -25,17 +26,22 @@ const NavigationBar = () => {
   };
 
   return (
-    <Navbar expand="md" fixed="top" expanded={isNavExpanded} className="shadow-sm"> 
+    <Navbar expand="md" fixed="top" expanded={isNavExpanded} className="shadow-sm">
       <Container>
-        <Navbar.Brand onClick={closeNav} className="text-muted" href="#home">Brad Hillier</Navbar.Brand>
+        <Navbar.Brand onClick={closeNav} className="text-muted" href="#home">
+          <div className="d-flex align-items-center">
+            <Image style={{ height: "35px" }} src="terminal-icon.svg" fluid />
+            <span>&nbsp;Brad Hillier</span>
+          </div>
+        </Navbar.Brand>
         <Navbar.Toggle onClick={handleNavToggle} aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mx-auto">
             {
               Object.keys(links).map(link => (
-                  <Nav.Link onClick={closeNav} className="mx-auto px-4" href={links[link]} key={link}>
-                    {link}
-                  </Nav.Link>
+                <Nav.Link onClick={closeNav} className="mx-auto px-4" href={links[link]} key={link}>
+                  {link}
+                </Nav.Link>
               ))
             }
           </Nav>
