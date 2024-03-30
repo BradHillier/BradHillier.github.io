a9f7d1d359abf28131d27c5d70894d5b484042bb
BradHillier <bradhillier@stumail.viu.ca>
2024-03-30 13:27:56 -0700

    Add individual components for each project
    Instead of using a single templated component, a new component has been
    created for each project — allowing for freedom to customize each
    project individually.

--- src/projects/project.js
+++ src/projects/project.js
@@ -1,49 +1,108 @@
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Stack from 'react-bootstrap/Stack'
+import Button from 'react-bootstrap/Button';
 import Image from 'react-bootstrap/Image';
 
 
-const Project = ({ image, title, description, buttons, color }) => {
-    return (
-        <Container style={{
-            backgroundColor: color,
-            borderRadius: "40px",
+const ProjectWrapper = ({ children }) => (
+        <Container className="project bg-light text-dark" style={{
+            borderRadius: "2em",
             margin: "20px 0px",
             padding: "40px",
             paddingBottom: "0px",
-            overflow: "hidden"
+            overflow: "hidden",
+            maxHeight: "80vh",
+            minHeight: "80vh"
         }}>
-            <Row className="text-center mb-5">
-                <h3>{title}</h3>
-            </Row>
-            <Row>
-                <Col lg={5}>
-                    <Stack>
-                        <p>{description}</p>
-                        <Stack direction="horizontal" gap={3}>
-                            {
-                                buttons.map((button, index) => (
-                                    <Button key={index} target="_blank" href={button.link} variant="outline-secondary">
-                                        <FontAwesomeIcon icon={button.icon} />
-                                        &nbsp;{button.label}
-                                    </Button>
-                                ))
-                            }
-                        </Stack>
-                    </Stack>
-                </Col>
-                <Col lg={7}>
-                    <Image src={image} style={{ borderRadius: "20px 20px 0px 0px" }} fluid/>
-                </Col>
-            </Row>
+            {children}
         </Container>
-    );
-}
 
-export default Project;
+);
+
+export const KayakRentalAPI = ({ image, title, description, buttons, color }) => (
+    <ProjectWrapper>
+        <Row className="text-center gy-4">
+            <h3>{title}</h3>
+        </Row>
+        <Row className="gy-4">
+            <Col lg={5}>
+                <Stack gap={1}>
+                    <p>{description}</p>
+                    <Stack direction="horizontal" gap={3}>
+                        {
+                            buttons.map((button, index) => (
+                                <Button key={index} target="_blank" href={button.link} variant="outline-secondary">
+                                    <FontAwesomeIcon icon={button.icon} />
+                                    &nbsp;{button.label}
+                                </Button>
+                            ))
+                        }
+                    </Stack>
+                </Stack>
+            </Col>
+            <Col lg={7}>
+                <Image src={image} style={{ borderRadius: "20px 20px 0px 0px" }} fluid/>
+            </Col>
+        </Row>
+    </ProjectWrapper>
+);
+
+
+export const Reminders = ({ image, title, description, buttons, color }) => (
+    <ProjectWrapper>
+        <Row className="d-flex flex-sm-row-reverse gy-5">
+            <Col lg={5} className="mx-auto d-flex align-items-center" style={{ maxHeight: "500px" }}>
+                <div>
+                    <h3 className="text-center text-dark">{title}</h3>
+                    <p>{description}</p>
+                    <Stack direction="horizontal" gap={3}>
+                    {
+                        buttons.map((button, index) => (
+                            <Button key={index} target="_blank" href={button.link} variant="outline-primary">
+                                <FontAwesomeIcon icon={button.icon} />
+                                &nbsp;{button.label}
+                            </Button>
+                        ))
+                    }
+                    </Stack>
+                </div>
+            </Col>
+            <Col lg={5}>
+                <Image src={image} fluid/>
+            </Col>
+        </Row>
+    </ProjectWrapper>
+);
+
+
+export const Chess = ({ image, title, description, buttons, color }) => (
+    <ProjectWrapper>
+        <Row>
+            <Col lg={6} className="mx-auto d-flex align-items-center" style={{ maxHeight: "500px" }}>
+                <div>
+                    <h3 className="text-center">{title}</h3>
+                    <p>{description}</p>
+                    <p>Designed and developed over the course of 4 months, as part of a team.</p>
+                    <Stack direction="horizontal" gap={3}>
+                    {
+                        buttons.map((button, index) => (
+                            <Button key={index} target="_blank" href={button.link} variant="outline-dark">
+                                <FontAwesomeIcon icon={button.icon} />
+                                &nbsp;{button.label}
+                            </Button>
+                        ))
+                    }
+                    </Stack>
+                </div>
+            </Col>
+            <Col lg={6}>
+                <Image src={image} style={{
+                    borderRadius: "20px 20px 0px 0px"
+                }} fluid/>
+            </Col>
+        </Row>
+    </ProjectWrapper>
+);
--- src/projects/projects.js
+++ src/projects/projects.js
@@ -1,33 +1,41 @@
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
 import Section from './../common/section';
 import ProjectTitle from './projectTitle.js';
-import Project from './project.js';
+import Project, { Reminders, Chess } from './project.js';
 import projects from './projects.json';
 import Rolodex from '../common/rolodex.js';
 
 
 const Projects = () => {
     const colors = [
-        "#EBBAB9",
-        "#388697",
+        "white",
+        "white",
         "#B5FFE1"
     ]
     return (
-        <Section id="projects" className="bg-light">
+        <Section id="projects" className="bg-white">
             <ProjectTitle />
             <Rolodex>
-                {projects.map((project, index) => (
-                        <Project 
-                            key={index}
-                            image={project.image}
-                            title={project.title}
-                            description={project.description}
-                            buttons={project.buttons}
-                            color={colors[index]}
-                        />
-                ))}
+                <Chess
+                    image={projects[2].image}
+                    title={projects[2].title}
+                    description={projects[2].description}
+                    buttons={projects[2].buttons}
+                    color={colors[1]}
+                />
+                <Project
+                    image={projects[0].image}
+                    title={projects[0].title}
+                    description={projects[0].description}
+                    buttons={projects[0].buttons}
+                    color={colors[0]}
+                />
+                <Reminders
+                    image={projects[1].image}
+                    title={projects[1].title}
+                    description={projects[1].description}
+                    buttons={projects[1].buttons}
+                    color={colors[2]}
+                />
             </Rolodex>
         </Section>
     );
