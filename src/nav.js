a8a2e38f8457c164624c4e585defc04271b8aa6c
BradHillier <bradhillier@stumail.viu.ca>
2024-04-13 00:05:45 -0700

    Add hide nav bar on scroll functionality
    Use scrollY to keep track of scroll direction by comparing the previous
    scrollY position with the current. If the current position is greater
    than the previous, hide the nav bar, otherwise show the nav bar.

--- src/nav.js
+++ src/nav.js
@@ -1,14 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 import { Image } from 'react-bootstrap';
-
+import { useScroll } from "framer-motion"
+import { motion } from 'framer-motion';
 
 const NavigationBar = () => {
 
   const [isNavExpanded, setIsNavExpanded] = useState(false);
+  const { scrollY } = useScroll()
+
 
   const links = {
     'Home': '#home',
@@ -25,32 +28,52 @@ const NavigationBar = () => {
     setIsNavExpanded(false);
   };
 
+  const [hidden, setHidden] = useState(false);
+
+  function update() {
+    if (scrollY?.current < scrollY?.prev) {
+      setHidden(false);
+    } else if (scrollY?.current > 100 && scrollY?.current > scrollY?.prev) {
+      setHidden(true);
+    }
+  }
+
+  const variants = {
+    visible: { y: 0 },
+    hidden: { y: -100 },
+  };
+
+  useEffect(() => {
+    return scrollY.onChange(() => update());
+  });
+
   return (
-    <Navbar expand="md" fixed="top" expanded={isNavExpanded} className="shadow-sm">
-      <Container>
-        <Navbar.Brand onClick={closeNav} className="text-muted" href="#home">
-          <div className="d-flex align-items-center">
-            <Image style={{ height: "35px" }} src="terminal-icon.svg" fluid />
-            <span>&nbsp;Brad Hillier</span>
-          </div>
-        </Navbar.Brand>
-        <Navbar.Toggle onClick={handleNavToggle} aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mx-auto">
-            {
-              Object.keys(links).map(link => (
-                <Nav.Link onClick={closeNav} className="mx-auto px-4" href={links[link]} key={link}>
-                  {link}
-                </Nav.Link>
-              ))
-            }
-          </Nav>
-          <Nav>
-            <Button onClick={closeNav} href="#contact" variant="outline-primary">Contact</Button>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <motion.nav className="shadow-sm navbar navbar-expand-md navbar-light fixed-top"
+        variants={variants}
+        animate={ hidden ? "hidden" : "visible" }
+        transition={{ ease: [0.1, 0.25, 0.3, 1], duration: 1 }}
+      >
+        <Container>
+          <Navbar.Brand onClick={closeNav} className="text-muted" href="#home">
+              <span>Brad Hillier</span>
+          </Navbar.Brand>
+          <Navbar.Toggle onClick={handleNavToggle} aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mx-auto">
+              {
+                Object.keys(links).map(link => (
+                  <Nav.Link onClick={closeNav} className="mx-auto px-4" href={links[link]} key={link}>
+                    {link}
+                  </Nav.Link>
+                ))
+              }
+            </Nav>
+            <Nav>
+              <Button onClick={closeNav} href="#contact" variant="outline-primary">Contact</Button>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </motion.nav>
   );
 }
 
