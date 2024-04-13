52c016d9495be0e2a39321f5670d3666f099828e
BradHillier <bradhillier@stumail.viu.ca>
2024-04-13 00:11:22 -0700

    Add more vertical padding to sections
    Add py-5 classNames of Section.

--- src/common/section.js
+++ src/common/section.js
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container';
 
 const Section = ({id, className, children}) => {
     return (
-        <section id={id} className={"py-5 ".concat(className)}> 
+        <section id={id} className={"my-5 py-5 ".concat(className)}> 
             <Container>
                 {children}
             </Container>
