4361036921e2f9ffa7040ff3e50b04afb5459bff
BradHillier <57161436+BradHillier@users.noreply.github.com>
2025-01-18 21:13:26 -0800

    refactor: temporarily remove rolodex component
    The rolodex component has several unaddressed bugs and has been deemed
    too broken to be used in production at this point. It will be re-added
    later once the bugs have been addressed.
    
    A simple animation free ProjectSection has been added to replace it.

--- src/features/projects/components/projects-section/projects-section.js
+++ src/features/projects/components/projects-section/projects-section.js
@@ -1,29 +1,51 @@
 import SectionHeadline from '../../../../components/ui/headline/headline.js';
 import { Project } from '../project/project.js';
 import projects from '../../assets/projects.json';
-import Rolodex from '../rolodex/rolodex.js';
+// import Rolodex from '../rolodex/rolodex.js';
 import { Container } from 'react-bootstrap';
 
+import './projects-section.scss';
+
+
+//const Projects = () => (
+    //<section id="projects" className="mt-5 pt-4">
+        //<Container>
+            //<SectionHeadline
+                //eyebrow="SEE WHAT I'M WORKING ON"
+                //headline="Personal Projects"
+                ///>
+            //<Rolodex
+                //stickyPosition={200}//132}
+                //finalOffset={120}
+                //visibleCardCount={5}
+                //visibleCardHeight={32}
+                //enableFade={true}
+                //gap={100}
+            //>
+                //{projects.map(project => 
+                    //<Project key={project.title} {...project} />
+                //)}
+            //</Rolodex>
+        //</Container>
+    //</section>
+//);
 
 const Projects = () => (
     <section id="projects" className="my-5 py-4">
         <Container>
-            <SectionHeadline
+            <SectionHeadline className="section-header"
                 eyebrow="SEE WHAT I'M WORKING ON"
                 headline="Personal Projects"
-                />
-            <Rolodex
-                stickyPosition={200}//132}
-                finalOffset={120}
-                visibleCardCount={5}
-                visibleCardHeight={32}
-                enableFade={true}
-                gap={100}
-            >
+            />
+            <div className="flex-container">
                 {projects.map(project => 
+                <div>
+                <div className="card-container">
                     <Project key={project.title} {...project} />
+                    </div>
+                    </div>
                 )}
-            </Rolodex>
+            </div>
         </Container>
     </section>
 );
