398a9f63ec87a26c29f0307916f936a371e83771
BradHillier <57161436+BradHillier@users.noreply.github.com>
2025-01-21 17:29:38 -0800

    fix: adjust class names for role and title

--- src/features/about-me/components/journey/journey.js
+++ src/features/about-me/components/journey/journey.js
@@ -6,9 +6,9 @@ const Journey = () => (
     <div class="timeline-box">
         <div class="timeline">
             {roles.map(role =>
-                <div class="milestone">
+                <div class="role">
                     <span class="year">{role.year}</span>
-                    <span class="event">{role.title}</span>
+                    <span class="title">{role.title}</span>
                 </div>
             )}
         </div>
